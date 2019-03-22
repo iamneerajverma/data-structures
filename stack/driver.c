1369316fbf82ed2e203187c289a7f854edc97e07
Neeraj Verma <neeraj.2132@gmail.com>
2019-03-22 13:08:41 +0530

    stack basics

---
+++ stack/driver.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include "stack.h"
+
+int generateRandomNumber(int max){
+	int value = rand()%max;
+	printf("%d ", value);
+	return value;
+}
+
+
+int main(){
+	int capacity = -1, max = -1, counter = 0;
+	printf("Please Enter no of values in the list:  _-_-_-   ");
+	scanf("%d", &capacity);
+	printf("\n\nPlease enter max value range  _-_-_-   ");
+	scanf("%d",&max);
+	printf("\n\n");
+	struct Stack* stack = createStack(capacity);
+	for(counter = 0; counter != capacity; counter++){
+		push(stack, generateRandomNumber(max));
+	}
+	printf("\n\n");
+	while (!isEmpty(stack)){
+		printf("%d ", pop(stack));
+	}
+	printf("\n\n");
+	return 0;
+}
---
+++ stack/stack.h
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+
+struct Stack{
+	int top;
+	int capacity;
+	int* array;
+};
+
+struct Stack* createStack(int capacity){
+	struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack*));
+	stack->top = -1;
+	stack->capacity = capacity;
+	stack->array = (int *) malloc(sizeof(int) * capacity);
+	return stack;
+}
+
+int isEmpty(struct Stack* stack){
+	return stack->top == -1;
+}
+
+int isFull(struct Stack* stack){
+	return stack->top == stack->capacity - 1;
+}
+
+void push(struct Stack* stack, int data){
+	if (isFull(stack)){
+		printf("Stack overflow! Please increase size.\n");
+	} else{
+		stack->array[++stack->top] = data;
+	}
+}
+
+int pop(struct Stack* stack){
+	if (isEmpty(stack)){
+		printf("Stack underflow!.\n");
+		return INT_MIN;
+	} else{
+		return stack->array[stack->top--];
+	}
+}
