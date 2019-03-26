89d6a98ec7c0ff7ac49e890d08e55cbb56074bf5
Neeraj Verma <neeraj.2132@gmail.com>
2019-03-26 18:36:17 +0530

    capacity and range dynamic in array-driver

--- array/driver.c
+++ array/driver.c
@@ -2,11 +2,14 @@
 #include "array.h"
 
 int main(){
-	int capacity = 25;
+	int capacity, max;
+	printf("Please enter size and max value for the array:  _-_-_-   ");
+	scanf("%d %d", &capacity, &max);
 	int* array = createArray(capacity);
-	fillWithRandomValues(array, capacity, 100);
+	fillWithRandomValues(array, capacity, max);
 	traverseArray(array, capacity);
-	bubbleSortArray(array, capacity);
+	selectionSortArray(array, capacity);
 	traverseArray(array, capacity);
+	printf("Array Sorted: %s\n\n", isArraySorted(array, capacity) ? "True" : "False");
 	return 0;
 }
